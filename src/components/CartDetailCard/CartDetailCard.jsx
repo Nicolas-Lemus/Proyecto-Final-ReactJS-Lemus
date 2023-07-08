3b5a3bc1c1f3f3b8adb0dd9ab1ab8a8998a85b7d
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-07-08 16:17:17 -0300

    quitamos descripcion en CartDetailCard

--- src/components/CartDetailCard/CartDetailCard.jsx
+++ src/components/CartDetailCard/CartDetailCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./CartDetailCard.css"
 
 const CartDetailCard = ({product, quantity}) => {
@@ -7,11 +6,10 @@ const CartDetailCard = ({product, quantity}) => {
   return (
     <div className="Card">
       <h3>{product.title}</h3>
-      <div className="Images"><Link to={`/products/${product.id}`}><img src={product.images} alt={product.title}/></Link></div>
+      <div className="Images"><img src={product.images} alt={product.title}/></div>
       <h5>c/u $ {product.price}</h5>
       <h6 className="qty">Cantidad : {quantity.quantity}</h6>
       <h6 id="total">Total: $ {product.price * quantity.quantity}</h6>
-      <p className="Detail"><Link to={`/products/${product.id}`}>ir a descripción</Link></p>
     </div>
   );
 };
